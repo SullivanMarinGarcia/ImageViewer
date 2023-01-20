db6ca2c431b34824d0278bb89e77b26750efdcdb
sgarc <sgarc@Sullivan_Laptop>
2023-01-20 17:41:16 +0100

    First version of imageviewer

---
+++ src/imageviewer/control/Main.java
@@ -0,0 +1,14 @@
+
+package imageviewer.control;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    
+
+        MainFrame mainFrame = new MainFrame();  
+        mainFrame.init();
+    }
+
+}
